7736e8a713a4be9fba6dc2471cfbdf87ffa2e34c
Kai Mallea <kmallea@gmail.com>
2011-09-21 10:38:38 -0400

    Initial commit

---
+++ tests.php
@@ -0,0 +1,35 @@
+<?php
+require "bbcode.php";
+
+$text=<<<EOF
+
+[b]This is bold text[/b]
+
+[i]This is italic text[/i]
+
+[code]This is pre-formatted text[/code]
+
+[quote]This is a quote[/quote]
+
+[color=red]This is red text[/color]
+
+[url]http://www.google.com/[/url]
+
+This is a list: 
+
+[list]
+[*]list item #1
+[*]list item #2
+[*][b]list item #3[/b]
+[/list]
+
+[youtube]http://youtu.be/DabwEqsWWiA&hd=1[/youtube]
+
+[youtube]http://www.youtube.com/watch?v=DabwEqsWWiA&hd=1[/youtube]
+
+
+EOF;
+
+$bbcode = new BBCode;
+echo $bbcode->toHTML($text);  
+?>
